637a33b92c1d7d36e91aa146b97d21dc2bcf3a77
Ronald Hecker <ronald.hecker@intel.com>
2024-10-24 14:48:43 +0200

    Use elevation instead of boxshadow

--- lib/pages/models/widgets/model_card.dart
+++ lib/pages/models/widgets/model_card.dart
@@ -2,6 +2,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:go_router/go_router.dart';
 import 'package:inference/pages/models/widgets/model_property.dart';
 import 'package:inference/project.dart';
+import 'package:inference/widgets/elevation.dart';
 
 class ModelCard extends StatefulWidget {
   final Project project;
@@ -20,9 +21,10 @@ class _ModelCardState extends State<ModelCard> with SingleTickerProviderStateMix
   void initState() {
     super.initState();
     controller = AnimationController(duration: const Duration(milliseconds: 300), vsync: this);
-    animation = Tween<double>(begin: 0, end: 3).animate(controller)..addListener(() {
+    animation = Tween<double>(begin: 2, end: 8).animate(controller)..addListener(() {
       setState(() {});
     });
+    controller.reset();
   }
 
   @override
@@ -39,66 +41,62 @@ class _ModelCardState extends State<ModelCard> with SingleTickerProviderStateMix
         onEnter: (_) => controller.forward(),
         onExit: (_) => controller.reverse(),
         cursor: SystemMouseCursors.click,
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(4)),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.12),
-                spreadRadius: animation.value,
-                blurRadius: 2,
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AspectRatio(
-                aspectRatio: 5/4,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: const Color(0x0D000000),
-                      width: 1,
-                    ),
-                    image: DecorationImage(
-                      image: widget.project.thumbnailImage(),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(widget.project.name,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        )
+        child: Elevation(
+          elevation: animation.value,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: const BorderRadius.all(Radius.circular(4)),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 5/4,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color(0x0D000000),
+                        width: 1,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 14),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ModelProperty(name: "Task", value: widget.project.taskName()),
-                            ModelProperty(name: "Architecture", value: widget.project.tasks[0].architecture.substring(0, 10)),
-                          ],
-                        ),
-                      )
-                    ],
+                      image: DecorationImage(
+                        image: widget.project.thumbnailImage(),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(widget.project.name,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          )
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 14),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ModelProperty(name: "Task", value: widget.project.taskName()),
+                              ModelProperty(name: "Architecture", value: widget.project.tasks[0].architecture.substring(0, 10)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
