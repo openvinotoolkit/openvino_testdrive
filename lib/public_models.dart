c02014151ba9c3037fb9a7d5d23439801e5fab70
Hecker, Ronald <ronald.hecker@intel.com>
2024-11-05 08:41:25 +0100

    Remove collection to private speech to text

--- lib/public_models.dart
+++ lib/public_models.dart
@@ -16,7 +16,6 @@ const huggingFaceURL = "https://huggingface.co";
 
 const List<Collection> collections = [
   Collection("https://huggingface.co/api/collections/OpenVINO/llm-6687aaa2abca3bbcec71a9bd", "", "text"),
-  //Collection("https://huggingface.co/api/collections/rhecker/speech-670ba88d40c7862e25913551", "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", "speech"),
 ];
 
 void createDirectory(PublicProject project) {
