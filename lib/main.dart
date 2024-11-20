20ea96da7424b59aedcb9f79cf67f1e76f80ec20
Kramer, Arend Jan <arend.jan.kramer@intel.com>
2024-11-20 15:15:05 +0100

    Attempt for build output validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:inference/config.dart';
+import 'package:inference/interop/device.dart';
 import 'package:inference/openvino_console_app.dart';
 import 'package:inference/providers/preference_provider.dart';
 import 'package:inference/providers/project_provider.dart';
 import 'package:inference/public_models.dart';
 import 'package:provider/provider.dart';
+import 'package:args/args.dart';
 
 void testConnection() async {
   final dio = Dio(BaseOptions(connectTimeout: Duration(seconds: 10)));
@@ -20,9 +24,32 @@ void testConnection() async {
   }
 }
 
-void main() {
-  testConnection();
-  runApp(const App());
+void main(List<String> arguments) async {
+  final parser = ArgParser()..addFlag('test-bindings', defaultsTo: false);
+
+  ArgResults argResults = parser.parse(arguments);
+  if (argResults.flag('test-bindings')) {
+    await testBindings();
+  } else {
+    testConnection();
+    runApp(const App());
+  }
+}
+
+Future<void> testBindings() async {
+  print("Testing bindings...");
+  try {
+    final devices = await Device.getDevices();
+    for (var device in devices) {
+      print("${device.id}, ${device.name}");
+    }
+    print("Bindings ok");
+    exit(0); // Exit with success
+  } catch (e) {
+    print("Error: $e");
+    print("Exit with failure");
+    exit(1); // Exit with failure
+  }
 }
 
 class App extends StatelessWidget {
