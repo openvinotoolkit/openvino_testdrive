15038c814990de2f08a69d4b743bf0243df35bf8
Ronald Hecker <ronald.hecker@intel.com>
2024-11-26 16:29:22 +0100

    Add crud for Knowledge Base groups

--- lib/pages/knowledge_base/knowledge_base.dart
+++ lib/pages/knowledge_base/knowledge_base.dart
@@ -3,6 +3,8 @@ import 'package:inference/langchain/object_box/embedding_entity.dart';
 import 'package:inference/langchain/object_box/object_box.dart';
 import 'package:inference/langchain/openvino_embeddings.dart';
 import 'package:inference/objectbox.g.dart';
+import 'package:inference/pages/models/widgets/grid_container.dart';
+import 'package:inference/theme_fluent.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -16,8 +18,11 @@ class KnowledgeBase extends StatefulWidget {
 class _KnowledgeBaseState extends State<KnowledgeBase> {
   final controller = TextEditingController();
   late Box<EmbeddingEntity> embeddingsBox;
+  late Box<KnowledgeGroup> groupBox;
   OpenVINOEmbeddings? embeddingsModel;
 
+  List<KnowledgeGroup> groups = [];
+  int? editId;
 
   void initEmbeddings() async {
     final platformContext = Context(style: Style.platform);
@@ -32,6 +37,12 @@ class _KnowledgeBaseState extends State<KnowledgeBase> {
     super.initState();
 
     embeddingsBox = ObjectBox.instance.store.box<EmbeddingEntity>();
+    groupBox = ObjectBox.instance.store.box<KnowledgeGroup>();
+    groupBox.getAllAsync().then((b) {
+        setState(() {
+            groups = b;
+        });
+    });
   }
 
   void test() async {
@@ -55,8 +66,150 @@ class _KnowledgeBaseState extends State<KnowledgeBase> {
     //});
   }
 
+  void renameGroup(KnowledgeGroup group, String value) {
+    setState(() {
+      editId = null;
+      groupBox.put(group..name = value);
+    });
+  }
+
+  void deleteGroup(KnowledgeGroup group) {
+    groupBox.remove(group.internalId);
+    setState(() {
+        groups.remove(group);
+    });
+  }
+
+  void addGroup() {
+    setState(() {
+      editId = groupBox.put(KnowledgeGroup("New group"));
+      groups = groupBox.getAll();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final theme = FluentTheme.of(context);
+
+    return GestureDetector(
+      behavior: HitTestBehavior.deferToChild,
+      onTap: () {
+        if (editId != null) {
+          setState(() {
+              editId = null;
+          });
+        }
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 280,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                GridContainer(
+                  color: backgroundColor.of(theme),
+                  padding: const EdgeInsets.all(16),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8),
+                  ),
+                  child: const Text("Knowledge Base",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GridContainer(
+                    color: backgroundColor.of(theme),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TreeView(
+                          items: [
+                            for (final group in groups)
+                              TreeViewItem(
+                                content: GroupItem(
+                                  editable: group.internalId == editId,
+                                  group: group,
+                                  onRename: (value) => renameGroup(group, value),
+                                  onMakeEditable: () {
+                                    setState(() {
+                                        editId = group.internalId;
+                                    });
+                                  },
+                                  onDelete: () => deleteGroup(group),
+                                )
+                              ),
+                          ]
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Button(
+                            onPressed: addGroup,
+                            child: const Text("Add group"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GroupItem extends StatefulWidget {
+  final KnowledgeGroup group;
+  final bool editable;
+  final Function(String)? onRename;
+  final Function()? onDelete;
+  final Function()? onMakeEditable;
+  const GroupItem({super.key, required this.group, required this.editable, this.onRename, this.onDelete, this.onMakeEditable});
+
+  @override
+  State<GroupItem> createState() => _GroupItemState();
+}
+
+class _GroupItemState extends State<GroupItem> {
+  final controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.text = widget.group.name;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.editable) {
+      return TextBox(
+        controller: controller,
+        onSubmitted: (value) {
+          widget.onRename?.call(value);
+        },
+      );
+    }
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onDoubleTap: () {
+        widget.onMakeEditable?.call();
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(widget.group.name),
+          IconButton(icon: const Icon(FluentIcons.delete_rows), onPressed: () {
+              widget.onDelete?.call();
+          }),
+        ],
+      ),
+    );
   }
 }
