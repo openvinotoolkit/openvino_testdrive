5f1caadd188a4a367ad04d1d45338e2d30828d14
Hecker, Ronald <ronald.hecker@intel.com>
2024-12-04 15:40:18 +0100

    Add import provider
    This is needed because design says the Import selected button is in
    navigation view action.

--- lib/pages/import/huggingface.dart
+++ lib/pages/import/huggingface.dart
@@ -1,10 +1,12 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:inference/importers/manifest_importer.dart';
+import 'package:inference/pages/import/providers/import_provider.dart';
 import 'package:inference/pages/import/widgets/badge.dart';
 import 'package:inference/pages/import/widgets/model_card.dart';
 import 'package:inference/widgets/controls/dropdown_multiple_select.dart';
 import 'package:inference/widgets/controls/search_bar.dart';
 import 'package:inference/widgets/fixed_grid.dart';
+import 'package:provider/provider.dart';
 
 class Huggingface extends StatefulWidget {
   const Huggingface({super.key});
@@ -16,16 +18,6 @@ class Huggingface extends StatefulWidget {
 class _HuggingfaceState extends State<Huggingface> {
   List<String> selectedOptimizations = [];
   String? searchValue;
-  late Future<List<Model>> allModelsFuture;
-  late Model? selectedModel;
-
-  @override
-  void initState() {
-    super.initState();
-    final importer = ManifestImporter('assets/manifest.json');
-    allModelsFuture = importer.loadManifest().then((_) => importer.getAllModels());
-    selectedModel = null;
-  }
 
   List<Model> filterModels(List<Model> models) {
     var filteredModels = models;
@@ -40,99 +32,101 @@ class _HuggingfaceState extends State<Huggingface> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: ConstrainedBox(constraints: const BoxConstraints(maxWidth: 1228),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 133, right: 80, top: 36),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  ConstrainedBox(
-                    constraints: const BoxConstraints(maxWidth: 280),
-                    child: Semantics(
-                      label: 'Find a model',
-                      child: SearchBar(onChange: (value) { setState(() {
-                        searchValue = value;
-                      }); }, placeholder: 'Find a model',),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    child: ConstrainedBox(
-                      constraints: const BoxConstraints(maxWidth: 184),
-                      child: DropdownMultipleSelect(
-                        items: const ['int4', 'int8', 'fp16'],
-                        selectedItems: selectedOptimizations,
-                        onChanged: (value) {
-                          if (!value.contains(selectedModel?.optimizationPrecision)) {
-                            selectedModel = null;
-                          }
-                          setState(() {
-                            selectedOptimizations = value;
-                          });
-                        },
-                        placeholder: 'Select optimizations',
+    return Consumer<ImportProvider>(builder: (context, importProvider, child) {
+      return SingleChildScrollView(
+        child: ConstrainedBox(constraints: const BoxConstraints(maxWidth: 1228),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 133, right: 80, top: 36),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 280),
+                      child: Semantics(
+                        label: 'Find a model',
+                        child: SearchBar(onChange: (value) { setState(() {
+                          searchValue = value;
+                        }); }, placeholder: 'Find a model',),
                       ),
                     ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 2),
-                child: SizedBox(
-                  height: 28,
-                  width: double.infinity,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Wrap(
-                      spacing: 8,
-                      children: selectedOptimizations.map((opt) {
-                        return Badge(text: opt, onDelete: () {
-                          if (opt == selectedModel?.optimizationPrecision && selectedOptimizations.length > 1) {
-                            selectedModel = null;
-                          }
-                          setState(() {
-                            selectedOptimizations.remove(opt);
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 184),
+                        child: DropdownMultipleSelect(
+                          items: const ['int4', 'int8', 'fp16'],
+                          selectedItems: selectedOptimizations,
+                          onChanged: (value) {
+                            if (!value.contains(importProvider.selectedModel?.optimizationPrecision)) {
+                              importProvider.selectedModel = null;
+                            }
+                            setState(() {
+                              selectedOptimizations = value;
+                            });
+                          },
+                          placeholder: 'Select optimizations',
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 2),
+                  child: SizedBox(
+                    height: 28,
+                    width: double.infinity,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Wrap(
+                        spacing: 8,
+                        children: selectedOptimizations.map((opt) {
+                          return Badge(text: opt, onDelete: () {
+                            if (opt == importProvider.selectedModel?.optimizationPrecision && selectedOptimizations.length > 1) {
+                              importProvider.selectedModel = null;
+                            }
+                            setState(() {
+                              selectedOptimizations.remove(opt);
+                            });
                           });
-                        });
-                      }).toList(),
+                        }).toList(),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              FutureBuilder<List<Model>>(
-                future: allModelsFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const ProgressRing();
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Text('No models available');
-                  } else {
-                    var allModels = filterModels(snapshot.data!);
-                    return FixedGrid(
-                      tileWidth: 226,
-                      spacing: 24,
-                      itemCount: allModels.length,
-                      itemBuilder: (context, index) => ModelCard(
-                        model: allModels[index],
-                        checked: selectedModel == allModels[index],
-                        onChecked: (value) {
-                          setState(() {
-                            selectedModel = value ? allModels[index] : null;
-                          });
-                        },
-                      ),
-                    );
-                  }
-                },
-              ),
-            ],
+                FutureBuilder<List<Model>>(
+                  future: importProvider.allModelsFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const ProgressRing();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Text('No models available');
+                    } else {
+                      var allModels = filterModels(snapshot.data!);
+                      return FixedGrid(
+                        tileWidth: 226,
+                        spacing: 24,
+                        itemCount: allModels.length,
+                        itemBuilder: (context, index) => ModelCard(
+                          model: allModels[index],
+                          checked: importProvider.selectedModel == allModels[index],
+                          onChecked: (value) {
+                            setState(() {
+                              importProvider.selectedModel = value ? allModels[index] : null;
+                            });
+                          },
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
--- lib/pages/import/import.dart
+++ lib/pages/import/import.dart
@@ -1,7 +1,9 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:go_router/go_router.dart';
 import 'package:inference/pages/import/huggingface.dart';
+import 'package:inference/pages/import/providers/import_provider.dart';
 import 'package:inference/pages/import/widgets/import_geti_model_dialog.dart';
+import 'package:provider/provider.dart';
 
 class ImportPage extends StatefulWidget {
   const ImportPage({super.key});
@@ -22,47 +24,97 @@ class _ImportPageState extends State<ImportPage> {
         ))
     );
 
-    return FluentTheme(
-      data: updatedTheme,
-      child: NavigationView(
-        pane: NavigationPane(
-          size: const NavigationPaneSize(topHeight: 64),
-          header: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: const Text("Import model",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return ChangeNotifierProvider<ImportProvider>(
+      create: (_) => ImportProvider(),
+      child: Stack(
+        children: [
+          FluentTheme(
+            data: updatedTheme,
+            child: NavigationView(
+              pane: NavigationPane(
+                size: const NavigationPaneSize(topHeight: 64),
+                header: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: const Text("Import model",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                selected: selected,
+                onChanged: (i) => setState(() {selected = i;}),
+                displayMode: PaneDisplayMode.top,
+                items: [
+                  PaneItem(
+                    icon: const Icon(FluentIcons.processing),
+                    title: const Text("Huggingface"),
+                    body: const  Huggingface(),
+                  ),
+                  PaneItemAction(
+                    icon: const Icon(FluentIcons.project_collection),
+                    title: const Text("Local disk"),
+                    onTap: () => showImportGetiModelDialog(context,
+                      callback: (projects) {
+                        if (projects != null && projects.isNotEmpty) {
+                          if (projects.length == 1) {
+                            GoRouter.of(context).go("/models/inference", extra: projects.first);
+                          } else {
+                            GoRouter.of(context).pop();
+                          }
+                        }
+                      }
+                    ),
+                  ),
+                ],
+              )
+            ),
           ),
-          selected: selected,
-          onChanged: (i) => setState(() {selected = i;}),
-          displayMode: PaneDisplayMode.top,
-          items: [
-            PaneItem(
-              icon: const Icon(FluentIcons.processing),
-              title: const Text("Huggingface"),
-              body: const  Huggingface(),
-            ),
-            PaneItemAction(
-              icon: const Icon(FluentIcons.project_collection),
-              title: const Text("Local disk"),
-              onTap: () => showImportGetiModelDialog(context,
-                callback: (projects) {
-                  if (projects != null && projects.isNotEmpty) {
-                    if (projects.length == 1) {
-                      GoRouter.of(context).go("/models/inference", extra: projects.first);
-                    } else {
-                      GoRouter.of(context).pop();
-                    }
-                  }
-                }
+          SizedBox(
+            height: 64,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Consumer<ImportProvider>(builder: (context, importProvider, child) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: FilledButton(
+                        onPressed: (importProvider.selectedModel == null
+                          ? null
+                          : () {
+                            importProvider.selectedModel?.convertToProject().then((project) {
+                              if (context.mounted){
+                                GoRouter.of(context).push('/models/download', extra: project);
+                              }
+                            });
+                          }
+                        ),
+                        child: const Text("Import selected model"),
+                      ),
+                    );
+                  }),
+                  Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                        shape:WidgetStatePropertyAll(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
+                          side:  const BorderSide(color: Color(0XFF545454)),
+                        )),
+                      ),
+                      child: const Text("Close"),
+                      onPressed: () =>  GoRouter.of(context).go("/models"),
+                    ),
+                  ),
+                ]
               ),
             ),
-          ],
-        )
+          )
+
+        ],
       ),
     );
   }
