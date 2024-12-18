88941508a40677e42fe7e60c3f7c99056374a528
Ronald Hecker <ronald.hecker@intel.com>
2024-12-18 15:26:29 +0100

    Use window manager
    macos has its own buttons, but windows and linux use same button to control

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // Copyright 2024 Intel Corporation.
 // SPDX-License-Identifier: Apache-2.0
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:fluent_ui/fluent_ui.dart';
@@ -10,12 +11,13 @@ import 'package:inference/providers/preference_provider.dart';
 import 'package:inference/providers/project_provider.dart';
 import 'package:inference/public_models.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 
 
 const String title = 'OpenVINO TestDrive';
 
 void testConnection() async {
-  final dio = Dio(BaseOptions(connectTimeout: Duration(seconds: 10)));
+  final dio = Dio(BaseOptions(connectTimeout: const Duration(seconds: 10)));
 
   try {
     await dio.get(collections[0].path);
@@ -27,8 +29,23 @@ void testConnection() async {
   }
 }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   testConnection();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    size: const Size(1400, 1024),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+    windowButtonVisibility: Platform.isMacOS,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(const App());
 }
 
