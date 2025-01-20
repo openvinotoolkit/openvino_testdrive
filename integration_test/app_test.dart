d042fcb7a1da518086404c6a73b970d54bbe825b
Dmitry Kalinin <dmitry.kalinin@intel.com>
2025-01-20 10:54:35 +0100

    Fixed imports

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:inference/main.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
