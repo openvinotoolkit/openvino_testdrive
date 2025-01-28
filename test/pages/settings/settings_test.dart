479f23d32bed6d4a6c975c1f01e9b94c1a6ea4a7
Dmitry Kalinin <dmitry.kalinin@intel.com>
2025-01-28 10:06:50 +0100

    Fixed headers

--- test/pages/settings/settings_test.dart
+++ test/pages/settings/settings_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:inference/config.dart';
@@ -6,7 +10,6 @@ import 'package:inference/theme_fluent.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:provider/provider.dart';
 
-import '../../mocks.dart';
 import '../../utils.dart';
 
 
