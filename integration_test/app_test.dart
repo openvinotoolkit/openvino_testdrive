faa43489546b1a00f571e18148371f8d20c46939
Dmitry Kalinin <dmitry.kalinin@intel.com>
2025-02-03 12:20:57 +0100

    MacOS build workflow for GitHub Actions (#82)
    * Enabled integration tests
    
    * Added multi-os build pipeline
    
    * Added permissions
    
    * Fixed workflow
    
    * Disabled other workflows
    
    * Changed shell to bash for windows
    
    * Fixed minor issues
    
    * Fixed workflow
    
    * FIxed workflow
    
    * Fixed workflow
    
    * Fixed workflow
    
    * Fixed workflow
    
    * Fixed workflow
    
    * Fixed workflow
    
    * Fixed windows cmake
    
    * Separated bundling step
    
    * Fixed ui build
    
    * debug
    
    * debug
    
    * Debug
    
    * Refactoring macOS library bundling and signing in proccess
    
    * Fixed build with ffmpeg
    
    * Fixed build step
    
    * First attempt on macos bindings build
    
    * WIP macos build
    
    * Added package step
    
    * Enabled workflow
    
    * Fixed filename in package step
    
    * Fixed trigger
    
    * Build fix
    
    * Fix build
    
    * Fixed build
    
    * Fixed build
    
    * Fixed typo
    
    * Removed debug
    
    * Fixed ffmpeg installation
    
    * Trying to fix linux build
    
    * Fix build
    
    * Fixed build
    
    * Combined package jobs
    
    * Included test jobs into main workflow
    
    * Fixed build
    
    * Fixed workflow
    
    * Fixed workflow
    
    * Fixed test workflow
    
    * Fixed imports
    
    * WIP on build
    
    * Fix linux build
    
    * Added cache, removed old CI
    
    * Fixed workflow
    
    * Fixed workflow
    
    * fixed cache
    
    * Fixed workflow
    
    * Fixed cache
    
    * Fixed workflow
    
    * Fixed workflow
    
    * Trying to fix cache
    
    * Fixed build
    
    * Extracted linux workflow
    
    * Fixed workflow
    
    * Fixed linux build
    
    * Trying to fix build
    
    * Fixed cache
    
    * Adjusted rights
    
    * Trying to fix build
    
    * Downgraded ubuntu on the  integration tests
    
    * Enabled windows build
    
    * Fixed cache

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,33 +2,35 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:inference/main.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  //testWidgets('Download model from HF', (tester) async {
-  //  const app = App();
-  //  await tester.pumpWidget(app);
+  testWidgets('Download model from HF', (tester) async {
+   const app = App();
+   await tester.pumpWidget(app);
 
-  //  await tester.tap(find.text('Import model'));
-  //  await tester.pumpAndSettle();
+   await tester.tap(find.text('Import model'));
+   await tester.pumpAndSettle();
 
-  //  await tester.tap(find.text('Hugging Face'));
-  //  await tester.pumpAndSettle();
+   await tester.tap(find.text('Hugging Face'));
+   await tester.pumpAndSettle();
 
-  //  final searchBarFinder = find.bySemanticsLabel('Find a model').first;
-  //  await tester.tap(searchBarFinder, warnIfMissed: false);
-  //  await tester.pumpAndSettle();
-  //  await tester.enterText(searchBarFinder, 'tiny');
-  //  await tester.pumpAndSettle();
+   final searchBarFinder = find.bySemanticsLabel('Find a model').first;
+   await tester.tap(searchBarFinder, warnIfMissed: false);
+   await tester.pumpAndSettle();
+   await tester.enterText(searchBarFinder, 'tiny');
+   await tester.pumpAndSettle();
 
-  //  await tester.tap(find.text('TinyLlama 1.1B Chat V1.0').first);
-  //  await tester.pumpAndSettle();
-  //  await tester.tap(find.text('Import selected model'));
-  //  await tester.pumpFrames(app, const Duration(seconds: 1));
-  //  expect(find.textContaining(RegExp(r'^[1-9][\d,]* MB$')), findsNWidgets(2));
+   await tester.tap(find.text('TinyLlama 1.1B Chat V1.0').first);
+   await tester.pumpAndSettle();
+   await tester.tap(find.text('Import selected model'));
+   await tester.pumpFrames(app, const Duration(seconds: 1));
+   expect(find.textContaining(RegExp(r'^[1-9][\d,]* MB$')), findsNWidgets(2));
 
-  //  await tester.pumpAndSettle();
-  //});
+   await tester.pumpAndSettle();
+  });
 }
