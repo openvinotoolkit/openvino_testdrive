29018fd06a6a3b65e376dadf4fa5acfe5cdf06f4
Ronald Hecker <ronald.hecker@intel.com>
2025-02-08 08:49:31 +0100

    Add  TextInferenceProvider tests

---
+++ test/pages/text_generation/playground_test.dart
@@ -0,0 +1,80 @@
+// Copyright (c) 2024 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import 'dart:async';
+
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:inference/interop/device.dart' show Device;
+import 'package:inference/interop/llm_inference.dart';
+import 'package:inference/interop/openvino_bindings.dart' show ModelResponse, Metrics;
+import 'package:inference/pages/text_generation/playground.dart';
+import 'package:inference/project.dart';
+import 'package:inference/providers/preference_provider.dart';
+import 'package:inference/providers/text_inference_provider.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+
+import '../../fixtures.dart';
+
+
+class MockLLMInference extends Mock implements LLMInference {}
+
+Widget renderWidget(TextInferenceProvider textInferenceProvider, PreferenceProvider preferences, Project project) {
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider.value(
+        value: preferences,
+      ),
+      ChangeNotifierProvider.value(
+        value: textInferenceProvider,
+      ),
+    ],
+    child: FluentApp(
+      home: ScaffoldPage(
+        content: Playground(project: project)
+      )
+    ),
+  );
+}
+
+PreferenceProvider get preferenceProvider {
+    final preferences = PreferenceProvider("AUTO");
+    PreferenceProvider.availableDevices = const [Device("AUTO","auto"), Device("CPU", "Test CPU")];
+    return preferences;
+}
+
+main() {
+  late MockLLMInference inference;
+
+  setUpAll(() {
+    inference = MockLLMInference();
+  });
+  //testWidgets('test something', (tester) async {
+  //  final provider = TextInferenceProvider(largeLanguageModel(), "CPU");
+  //  provider.inference = inference;
+  //  provider.loaded.complete();
+
+  //  final completer = Completer<void>();
+
+  //  final metrics = calloc<Metrics>();
+  //  when(() => inference.prompt(any(), any(), any())).thenAnswer((_) async {
+  //    await completer.future;
+  //    return ModelResponse("The color of the sun is yellow", metrics.ref);
+
+  //  });
+
+  //  await tester.pumpWidget(renderWidget(provider, preferenceProvider, largeLanguageModel()));
+
+  //  await tester.enterText(find.byType(TextBox), 'What is the color of the sun?');
+
+
+
+  //  calloc.free(metrics);
+  //});
+
+
+}
