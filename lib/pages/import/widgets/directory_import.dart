8c155335124162bef464214215875cb30703d50f
Hecker, Ronald <ronald.hecker@intel.com>
2025-03-12 10:18:34 +0100

    Enable the drop area for directory dropping
    If the user drops a file it will take parent directory

--- lib/pages/import/widgets/directory_import.dart
+++ lib/pages/import/widgets/directory_import.dart
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+import 'dart:io';
+
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:fluent_ui/fluent_ui.dart';
@@ -9,6 +11,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:inference/importers/model_directory_importer.dart';
 import 'package:inference/providers/project_provider.dart';
 import 'package:inference/theme_fluent.dart';
+import 'package:path/path.dart' show dirname;
 import 'package:provider/provider.dart';
 
 class DirectoryImport extends StatefulWidget {
@@ -29,27 +32,29 @@ class _DirectoryImportState extends State<DirectoryImport> {
     setState(() => _showReleaseMessage = false);
   }
 
+  Future<void> selectFolder() async {
+    final directory = await FilePicker.platform.getDirectoryPath();
+    if (directory != null) {
+      processFolder(directory);
+    } else {
+      // User canceled the picker
+    }
+  }
+
+  Future<void> processFolder(String directory) async {
+    final importer = ModelDirImporter(directory);
+    final project = await importer.generateProject();
+    importer.setupFiles();
+    if (mounted) {
+      final projectsProvider = Provider.of<ProjectProvider>(context, listen: false);
+      projectsProvider.addProject(project);
+      Navigator.pop(context, [project]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = FluentTheme.of(context);
-
-    Future<void> selectFolder() async {
-      final result = await FilePicker.platform.getDirectoryPath();
-      if (result != null) {
-        print(result);
-        final importer = ModelDirImporter(result);
-        final project = await importer.generateProject();
-        importer.setupFiles();
-        if (context.mounted) {
-          final projectsProvider = Provider.of<ProjectProvider>(context, listen: false);
-          projectsProvider.addProject(project);
-          Navigator.pop(context, [project]);
-        }
-        //processFiles(result.files.map((p) => p.path!).toList());
-      } else {
-        // User canceled the picker
-      }
-    }
 
     return Container(
       color: backgroundColor.of(theme),
@@ -62,7 +67,14 @@ class _DirectoryImportState extends State<DirectoryImport> {
 
           return DropTarget(
             onDragDone: (details) {
-              //processFiles(details.files.map((f) => f.path).toList());
+              if (details.files.isNotEmpty) {
+                final target = details.files.first.path;
+                if (Directory(target).existsSync()) {
+                  processFolder(target);
+                } else {
+                  processFolder(dirname(target));
+                }
+              }
             },
             onDragExited: (val) => hideReleaseMessage(),
             onDragEntered: (val) => showReleaseMessage(),
