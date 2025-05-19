56b582dd5ef2a415bf14c50c81e91e8903b0ccf2
Ronald Hecker <ronald.hecker@intel.com>
2025-05-19 07:31:28 +0200

    Working scratch

--- openvino_bindings/src/scratch.cc
+++ openvino_bindings/src/scratch.cc
@@ -1,10 +1,16 @@
 #include "tti/tti_inference.h"
 #include <iostream>
+#include <thread>
+
+
+//void start_model(TTIInference& tti) {
+//    tti.prompt("Piano", 64, 64, 4);
+//}
 
 int main() {
     std::cout << "init" << std::endl;
 
-    std::string model_path = "C:/Users/selse/AppData/Roaming/intel.openvino/OpenVINO\ Test\ Drive/LCM_Dreamshaper_v7-int8-ov";
+    std::string model_path = "/mnt/c/Users/selse/AppData/Roaming/intel.openvino/OpenVINO\ Test\ Drive/LCM_Dreamshaper_v7-int8-ov";
 
 
     std::cout << "loading " << model_path << std::endl;
@@ -15,15 +21,21 @@ int main() {
 
     auto lambda_callback = [&tti](const StringWithMetrics& response, int n, int n_steps) {
         std::cout << "got " << n << " of " << n_steps << std::endl;
-
-        tti.force_stop();
     };
 
 
     tti.set_streamer(lambda_callback);
-    tti.prompt("Piano", 64, 64, 4);
+    std::cout << "starting prompt" << std::endl;
+    std::thread start_t(&TTIInference::prompt, &tti, "Piano", 64, 64, 4);
+    //tti.prompt("Piano", 64, 64, 4);
+    //
 
+    std::cout << "sleeping prompt" << std::endl;
+    std::this_thread::sleep_for (std::chrono::seconds(1));
+    std::cout << "forcing stop" << std::endl;
+    tti.force_stop();
+    std::cout << "joining prompt thread" << std::endl;
+    start_t.join();
     std::cout << "done" << std::endl;
-
     return 0;
 }
